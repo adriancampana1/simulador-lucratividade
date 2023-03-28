bcd4a361194d811406a3b014b929b4728e9ae89a
Adrian Campana <adrianphcampana12@gmail.com>
2023-03-28 10:01:45 -0300

    atualizacoes no css e javascript

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 const selectProduct = document.querySelector('#select-product');
 const quantMaquinas = document.querySelector('#quant-maquinas');
-const kitInstalacao = document.querySelector('#kit-instalacao');
-const maoDeObra = document.querySelector('#mao-de-obra');
-const aguaLuz = document.querySelector('#agua-luz');
-const notaFiscal = document.querySelector('#nota-fiscal');
+
 const valorNegociacao = document.querySelector('#valor-negociacao');
 const formaDePagamento = document.querySelector('#forma-pagamento');
 const btnForm = document.querySelector('.form button');
@@ -47,12 +44,88 @@ function handleKeyDown(event) {
 form.onsubmit = (event) => {
     event.preventDefault();
 
+    const product = selectProduct.options[selectProduct.selectedIndex].value;
+    const formaDePagamentoSelecionada =
+        formaDePagamento.options[formaDePagamento.selectedIndex].value;
+
+    const kitInstalacao = document.querySelector(
+        'input[name="kit-instalacao"]:checked'
+    ).value;
+    const maoDeObra = document.querySelector(
+        'input[name="mao-de-obra"]:checked'
+    ).value;
+    const aguaLuz = document.querySelector(
+        'input[name="agua-luz"]:checked'
+    ).value;
+    const notaFiscal = document.querySelector(
+        'input[name="nota-fiscal"]:checked'
+    ).value;
+
+    const valorDaNegociacao = valorNegociacao.value;
+    const quantidadeDeMaquinas = quantMaquinas.value;
+
+    calculadoraDeLucratividade(product, formaDePagamentoSelecionada);
+
     Modal.openModal();
     Modal.closeScreen();
-    /* displayProfitabilityResult(result); */
+
+    displayProfitabilityResult(result);
 };
 
 /* Funções */
+
+function calculadoraDeLucratividade(
+    product,
+    formaDePagamentoSelecionada,
+    kitInstalacao,
+    maoDeObra,
+    aguaLuz,
+    notaFiscal,
+    valorDaNegociacao,
+    quantidadeDeMaquinas
+) {
+    let custoDoProduto = 0;
+    let taxaDePagamento = 0;
+    let custoKitInstalacao = 1400;
+    let custoMaoDeObraInstalacao = 700;
+    let custoMaoDeObraEnergiaeAgua = 300;
+    let custoImposto = 0.03;
+
+    let custoTotalPadrao =
+        custoKitInstalacao +
+        custoMaoDeObraInstalacao +
+        custoMaoDeObraEnergiaeAgua;
+    let custoTotalFinal = 0;
+
+    switch (product) {
+        case 'teto-poloclima':
+            custoDoProduto = 3900;
+            break;
+        case 'teto-mwm':
+            custoDoProduto = 4500;
+            break;
+        case 'portatil-poloclima':
+            custoDoProduto = 4350;
+            break;
+        case false:
+            console.log('deu errado');
+    }
+
+    switch (formaDePagamentoSelecionada) {
+        case 'parcelamento-cartao':
+            taxaDePagamento = 12;
+            break;
+        case 'cheque':
+            taxaDePagamento = 2;
+            break;
+        case 'boleto':
+            break;
+        case 'a-vista':
+            break;
+        case false:
+            break;
+    }
+}
 
 function displayProfitabilityResult(result) {
     const message = `A lucratividade total é ${result}`;
