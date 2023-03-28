adf7b30e7a8eae9b383dcdf24707c0e5bdcb0604
Adrian Campana <adrianphcampana12@gmail.com>
2023-03-28 20:15:48 -0300

    finalizacao de funcionalidades

--- js/index.js
+++ js/index.js
@@ -1,16 +1,23 @@
+import { alertError, campoVazio } from './alert-error.js';
+
 const selectProduct = document.querySelector('#select-product');
 const quantMaquinas = document.querySelector('#quant-maquinas');
 
 const valorNegociacao = document.querySelector('#valor-negociacao');
 const formaDePagamento = document.querySelector('#forma-pagamento');
-const btnForm = document.querySelector('.form button');
+
 const form = document.querySelector('form');
 
 /* Modal */
 const Modal = {
     wrapper: document.querySelector('.modal-wrapper'),
     screen1: document.querySelector('.screen1'),
-    message: document.querySelector('.box-wrapper span'),
+    messagePorcentagem: document.querySelector('.porcentagem span'),
+    messageLucro: document.querySelector('.lucro span '),
+    messageValorTotalNegociacao: document.querySelector(
+        '.valor-da-negociacao-total span'
+    ),
+    messageCusto: document.querySelector('.custo span'),
     btnClose: document.querySelector('.modal button'),
 
     openModal() {
@@ -41,6 +48,7 @@ function handleKeyDown(event) {
 }
 
 /* Eventos */
+
 form.onsubmit = (event) => {
     event.preventDefault();
 
@@ -51,85 +59,136 @@ form.onsubmit = (event) => {
     const kitInstalacao = document.querySelector(
         'input[name="kit-instalacao"]:checked'
     ).value;
+
     const maoDeObra = document.querySelector(
         'input[name="mao-de-obra"]:checked'
     ).value;
+
     const aguaLuz = document.querySelector(
         'input[name="agua-luz"]:checked'
     ).value;
+
     const notaFiscal = document.querySelector(
         'input[name="nota-fiscal"]:checked'
     ).value;
 
-    const valorDaNegociacao = valorNegociacao.value;
-    const quantidadeDeMaquinas = quantMaquinas.value;
-
-    calculadoraDeLucratividade(product, formaDePagamentoSelecionada);
+    const valorDaNegociacao = parseInt(valorNegociacao.value);
+    const quantidadeDeMaquinas = parseInt(quantMaquinas.value);
 
     Modal.openModal();
     Modal.closeScreen();
 
-    displayProfitabilityResult(result);
+    calculadoraDeCustos(
+        product,
+        quantidadeDeMaquinas,
+        kitInstalacao,
+        maoDeObra,
+        aguaLuz,
+        notaFiscal,
+        valorDaNegociacao,
+        formaDePagamentoSelecionada
+    );
 };
 
 /* Funções */
 
-function calculadoraDeLucratividade(
+function calculadoraDeCustos(
     product,
-    formaDePagamentoSelecionada,
+    quantidadeDeMaquinas,
     kitInstalacao,
     maoDeObra,
     aguaLuz,
     notaFiscal,
     valorDaNegociacao,
-    quantidadeDeMaquinas
+    formaDePagamentoSelecionada
 ) {
-    let custoDoProduto = 0;
-    let taxaDePagamento = 0;
-    let custoKitInstalacao = 1400;
-    let custoMaoDeObraInstalacao = 700;
-    let custoMaoDeObraEnergiaeAgua = 300;
-    let custoImposto = 0.03;
-
-    let custoTotalPadrao =
-        custoKitInstalacao +
-        custoMaoDeObraInstalacao +
-        custoMaoDeObraEnergiaeAgua;
-    let custoTotalFinal = 0;
+    let custo = 0;
+    let taxas = 0;
 
     switch (product) {
         case 'teto-poloclima':
-            custoDoProduto = 3900;
+            custo = 3900;
+            custo = custo * parseInt(quantidadeDeMaquinas);
             break;
         case 'teto-mwm':
-            custoDoProduto = 4500;
+            custo = 4500;
+            custo = custo * quantidadeDeMaquinas;
             break;
         case 'portatil-poloclima':
-            custoDoProduto = 4350;
+            custo = 4350;
+            custo = custo * quantidadeDeMaquinas;
             break;
         case false:
             console.log('deu errado');
     }
 
+    switch (kitInstalacao) {
+        case 'sim':
+            custo = custo + 1400;
+            break;
+        case 'nao':
+            break;
+        case false:
+            console.log('deu errado kit instalacao');
+    }
+
+    switch (maoDeObra) {
+        case 'sim':
+            custo = custo + 700;
+            break;
+        case 'nao':
+            break;
+        case false:
+            console.log('deu errado mao de obra');
+    }
+
+    switch (aguaLuz) {
+        case 'sim':
+            custo = custo + 300;
+            break;
+        case 'nao':
+            break;
+        case false:
+            console.log('deu errado agua e luz');
+    }
+
     switch (formaDePagamentoSelecionada) {
         case 'parcelamento-cartao':
-            taxaDePagamento = 12;
+            taxas = taxas + custo * 0.13;
+            custo = custo + taxas;
             break;
         case 'cheque':
-            taxaDePagamento = 2;
             break;
         case 'boleto':
             break;
         case 'a-vista':
             break;
         case false:
+            console.log('deu errado forma de pagamento');
             break;
     }
-}
 
-function displayProfitabilityResult(result) {
-    const message = `A lucratividade total é ${result}`;
+    switch (notaFiscal) {
+        case 'sim':
+            taxas = valorDaNegociacao * 0.03;
+            custo = custo + taxas;
+            break;
+        case 'nao':
+            break;
+        case false:
+            console.log('deu errado nota fiscal');
+    }
 
-    Modal.message.innerText = message;
-    Modal.open();
+    let lucroFinal = valorDaNegociacao - custo;
+    let porcentagemDeLucro = 100 - (custo / valorDaNegociacao) * 100;
+
+    const messageValorTotal = `R$${valorDaNegociacao.toFixed(2)}`;
+    const messagePorcentagemLucro = `${porcentagemDeLucro.toFixed(0)}%`;
+    const messageLucro = `R$${lucroFinal.toFixed(2)}`;
+    const messageCusto = `R$${custo.toFixed(2)}`;
+
+    Modal.messageValorTotalNegociacao.innerText = messageValorTotal;
+    Modal.messagePorcentagem.innerText = messagePorcentagemLucro;
+    Modal.messageLucro.innerText = messageLucro;
+    Modal.messageCusto.innerText = messageCusto;
 }
